d19c34e196cf8b939aeccf157afb31a1056f7a9b
david chapman <chap6680@gmail.com>
2018-10-28 12:58:54 -0600

    done with MVC

--- models/product.js
+++ models/product.js
@@ -1,15 +1,37 @@
-const products = [];
+const fs = require('fs');
+
+const path = require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json'
+);
+
+
+const getProductsFromFile = (callback) => { 
+	//return products;
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+			return callback([]);
+		} else {
+			callback(JSON.parse(fileContent));
+		};
+	});
+}
 
 module.exports = class Product { 
 	constructor(t) { 
 		this.title = t;
 	}
 
-	save() { 
-		products.push(this);
+	save() {
+		getProductsFromFile(products => {
+			products.push(this);
+			fs.writeFile(p, JSON.stringify(products), err => {
+				console.log(err);
+			});
+		});
 	}
 
-	static fetchAll() { 
-		return products;
+	static fetchAll(callback) { 
+		getProductsFromFile(callback);
 	}
 }
